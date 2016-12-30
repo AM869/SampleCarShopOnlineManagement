3bad820d8c442255b5d71f0c2ffc31b9bcdd3df7
AM869 <klsoukas@gmail.com>
2016-12-30 10:48:44 +0200

    cleaning

--- src/main/webapp/jsfolder/googleSignInOut.js
+++ src/main/webapp/jsfolder/googleSignInOut.js
@@ -13,16 +13,8 @@ function loadAndInitAuth2(){
 
 function onGoogleSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
         
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-    
     
     var xhr = new XMLHttpRequest();
     xhr.open('POST','https://localhost:8443/SampleCarShopOnlineManagement/TokenSignIn');
